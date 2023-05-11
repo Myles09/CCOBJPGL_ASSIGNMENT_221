e7d1f680653911e1c344ce52b1b769ea6473d8b7
Justin Cañedo <111473062+Myles09@users.noreply.github.com>
2023-05-11 10:01:31 +0800

    Update App.java

--- exercise3/App.java
+++ exercise3/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Create object from pet class
+        Sheshedogi Medoggy = New Sheshedogi();
+
+        Medoggy.bark();
+        System.out.println(Medoggy.mouth);
+        System.out.println(Medoggy.language);
     }
 }
